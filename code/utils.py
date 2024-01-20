a914e39a2d86998bb7b25203bd36eba80ebea3b8
parisashafa <parisa.shafa@gmail.com>
2024-01-20 18:11:03 +0330

    add read_data function to utils file

--- code/utils.py
+++ code/utils.py
@@ -1,1 +1,13 @@
+import numpy as np
+import pandas as pd
 
+def read_data():
+    A = pd.read_csv('G:/dissertation/important/LAGCN/data/drug_dis.csv', header=None).values
+    Sr = pd.read_csv('G:/dissertation/important/LAGCN/data/drug_sim.csv', header=None).values
+    Sd = pd.read_csv('G:/dissertation/important/LAGCN/data/dis_sim.csv', header=None).values
+
+    A_np = np.array(A)
+    Sr_np = np.array(Sr)
+    Sd_np = np.array(Sd)
+
+    return A_np, Sr_np, Sd_np
